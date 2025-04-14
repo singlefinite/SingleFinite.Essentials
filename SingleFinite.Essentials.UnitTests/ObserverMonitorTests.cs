a1931d55d0e931d80262033458f68fdc13e43cdb
Nate Wallace <natewallace@gmail.com>
2025-04-14 19:45:18 -0400

    Add ObserverMonitor.

---
+++ SingleFinite.Essentials.UnitTests/ObserverMonitorTests.cs
@@ -0,0 +1,156 @@
+﻿// MIT License
+// Copyright (c) 2025 Single Finite
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights 
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+// copies of the Software, and to permit persons to whom the Software is 
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in 
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+namespace SingleFinite.Essentials.UnitTests;
+
+[TestClass]
+public class ObserverMonitorTests
+{
+    [TestMethod]
+    public void Dispose_Is_Called_When_Owner_Removed()
+    {
+        var monitor = new ObserverMonitor();
+
+        var owner1 = new object();
+        var source1 = new ObservableSource<int>();
+        var source2 = new ObservableSource<string>();
+
+        var owner2 = new object();
+        var source3 = new ObservableSource<bool>();
+        var source4 = new ObservableSource<double>();
+
+        var observedArgs = new List<object>();
+
+        var observer1 = source1.Observable
+            .Observe()
+            .OnEach(args => observedArgs.Add(args));
+        var observer2 = source2.Observable
+            .Observe()
+            .OnEach(args => observedArgs.Add(args));
+        var observer3 = source3.Observable
+            .Observe()
+            .OnEach(args => observedArgs.Add(args));
+        var observer4 = source4.Observable
+            .Observe()
+            .OnEach(args => observedArgs.Add(args));
+
+        monitor.Add(
+            owner: owner1,
+            observer1,
+            observer2
+        );
+        monitor.Add(
+            owner: owner2,
+            observer3,
+            observer4
+        );
+
+        source1.Emit(9);
+        source2.Emit("HI");
+        source3.Emit(true);
+        source4.Emit(5.7);
+
+        Assert.AreEqual(4, observedArgs.Count);
+        Assert.AreEqual(9, observedArgs[0]);
+        Assert.AreEqual("HI", observedArgs[1]);
+        Assert.AreEqual(true, observedArgs[2]);
+        Assert.AreEqual(5.7, observedArgs[3]);
+
+        observedArgs.Clear();
+
+        monitor.Remove(owner1);
+
+        Assert.IsTrue(observer1.IsDisposed);
+        Assert.IsTrue(observer2.IsDisposed);
+        Assert.IsFalse(observer3.IsDisposed);
+        Assert.IsFalse(observer4.IsDisposed);
+
+        source1.Emit(9);
+        source2.Emit("HI");
+        source3.Emit(true);
+        source4.Emit(5.7);
+
+        Assert.AreEqual(2, observedArgs.Count);
+        Assert.AreEqual(true, observedArgs[0]);
+        Assert.AreEqual(5.7, observedArgs[1]);
+    }
+
+    [TestMethod]
+    public void Clear_Disposes_Of_All_Observers()
+    {
+        var monitor = new ObserverMonitor();
+
+        var owner1 = new object();
+        var source1 = new ObservableSource<int>();
+        var source2 = new ObservableSource<string>();
+
+        var owner2 = new object();
+        var source3 = new ObservableSource<bool>();
+        var source4 = new ObservableSource<double>();
+
+        var observedArgs = new List<object>();
+
+        var observer1 = source1.Observable
+            .Observe()
+            .OnEach(args => observedArgs.Add(args));
+        var observer2 = source2.Observable
+            .Observe()
+            .OnEach(args => observedArgs.Add(args));
+        var observer3 = source3.Observable
+            .Observe()
+            .OnEach(args => observedArgs.Add(args));
+        var observer4 = source4.Observable
+            .Observe()
+            .OnEach(args => observedArgs.Add(args));
+
+        monitor.Add(owner1, observer1);
+        monitor.Add(owner1, observer2);
+        monitor.Add(owner2, observer3);
+        monitor.Add(owner2, observer4);
+
+        source1.Emit(9);
+        source2.Emit("HI");
+        source3.Emit(true);
+        source4.Emit(5.7);
+
+        Assert.AreEqual(4, observedArgs.Count);
+        Assert.AreEqual(9, observedArgs[0]);
+        Assert.AreEqual("HI", observedArgs[1]);
+        Assert.AreEqual(true, observedArgs[2]);
+        Assert.AreEqual(5.7, observedArgs[3]);
+
+        observedArgs.Clear();
+
+        monitor.Clear();
+
+        Assert.IsTrue(observer1.IsDisposed);
+        Assert.IsTrue(observer2.IsDisposed);
+        Assert.IsTrue(observer3.IsDisposed);
+        Assert.IsTrue(observer4.IsDisposed);
+
+        source1.Emit(9);
+        source2.Emit("HI");
+        source3.Emit(true);
+        source4.Emit(5.7);
+
+        Assert.AreEqual(0, observedArgs.Count);
+    }
+}
---
+++ SingleFinite.Essentials/ObserverMonitor.cs
@@ -0,0 +1,86 @@
+﻿// MIT License
+// Copyright (c) 2025 Single Finite
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights 
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+// copies of the Software, and to permit persons to whom the Software is 
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in 
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+namespace SingleFinite.Essentials;
+
+/// <summary>
+/// Monitor observers by owner.  When an owner is removed from this object
+/// all the observers that are monitored by this object that belong to the
+/// owner will be disposed.
+/// </summary>
+public sealed class ObserverMonitor
+{
+    #region Fields
+
+    /// <summary>
+    /// Holds the list of observers by owner.
+    /// </summary>
+    private readonly Dictionary<object, List<IDisposable>> _observerDictionary = [];
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Add an observer to monitor for the given owner.
+    /// </summary>
+    /// <param name="owner">The owner of the observer.</param>
+    /// <param name="observers">The observers to monitor.</param>
+    public void Add(object owner, params IEnumerable<IDisposable> observers)
+    {
+        if (_observerDictionary.TryGetValue(owner, out var observerList))
+            observerList.AddRange(observers);
+        else
+            _observerDictionary.Add(owner, [.. observers]);
+    }
+
+    /// <summary>
+    /// Remove the owner from this object and dispose of all observers that
+    /// belong to the given owner.
+    /// </summary>
+    /// <param name="owner">The owner to remove from the monitor.</param>
+    public void Remove(object owner)
+    {
+        if (_observerDictionary.Remove(owner, out var observers))
+        {
+            foreach (var observer in observers)
+                observer.Dispose();
+        }
+    }
+
+    /// <summary>
+    /// Remove all owners from this object and dispose of all observers that
+    /// were being monitored.
+    /// </summary>
+    public void Clear()
+    {
+        var allObservers = _observerDictionary.Values
+            .SelectMany(observers => observers)
+            .ToList();
+
+        _observerDictionary.Clear();
+
+        foreach (var observer in allObservers)
+            observer.Dispose();
+    }
+
+    #endregion
+}
